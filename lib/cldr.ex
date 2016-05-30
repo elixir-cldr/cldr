fa42a545ddb6d519014e83301097b8f8d53f5b49
Kip Cole <kipcole9@gmail.com>
2016-05-30 19:25:29 +0900

    Add type specs and utility functions before we get into writing functions

--- lib/cldr.ex
+++ lib/cldr.ex
@@ -6,20 +6,71 @@
 defmodule Cldr.Rbnf do
   import Xml
   alias Cldr.Rbnf.Rule
-
-  def f do
-    "/Users/kip/Development/databases/cldr/common/rbnf/en.xml"
+  defdelegate parse(filename), to: Xml
+  @rbnf_dir Path.join(__DIR__, "/../data/common/rbnf")
+  
+  @spec rbnf_dir :: String.t
+  def rbnf_dir do
+    @rbnf_dir
   end
   
+  @spec locale_path(binary) :: String.t
+  def locale_path(locale) when is_binary(locale) do
+    Path.join(rbnf_dir, "/#{locale}.xml")
+  end
+  
+  @doc """
+  Returns a boolean identifying if the specified locale
+  is available in rbnf
+  """
+  @spec locale_exists?(String.t) :: boolean
+  def locale_exists?(locale) when is_binary(locale) do
+    locale_path(locale)
+    |> File.exists?
+  end
+  
+  @doc """
+  Returns a list of the configured locales for rbnf
+  
+  Locales are configured in `config.exs` 
+  
+      config :cldr,
+        locales: ["en", "fr"]
+        
+  It's also possible to use the locales from a Gettext
+  configuration:
+  
+      config :cldr,
+        gettext: App.Gettext
+  """
+  @spec configured_locales :: [String.t]
+  def configured_locales do
+    Application.get_env(:cldr, :locales)
+  end
+  
+  {:ok, files} = File.ls(@rbnf_dir)
+  @locales Enum.map(files, &Path.basename(&1, ".xml"))
+  
+  @doc """
+  Returns a list of the locales defined in rbnf
+  """
+  @spec known_locales :: [String.t]
+  def known_locales do
+    @locales
+  end
+  
+  @spec rulegroups(Xml.xml_node, String.t) :: [String.t]
   def rulegroups(xml, path \\ "//rulesetGrouping") do
     Enum.map(all(xml, path), fn(node) -> attr(node, "type") end)
   end
   
+  @spec rulesets(Xml.xml_node, binary) :: list([type: String.t, access: String.t])
   def rulesets(xml, rulegroup) do
     path = "//rulesetGrouping[@type='#{rulegroup}']/ruleset"
     Enum.map(all(xml, path), fn(node) -> [attr(node, "type"), attr(node, "access") || "public"] end)
   end
   
+  @spec rules(Xml.xml_node, String.t, String.t) :: [%Rule{}]
   def rules(xml, rulegroup, ruleset) do
     path = "//rulesetGrouping[@type='#{rulegroup}']/ruleset[@type='#{ruleset}']/rbnfrule"
     Enum.map(all(xml, path), fn(node) -> 
@@ -28,21 +79,28 @@ defmodule Cldr.Rbnf do
     |> set_range
   end
   
-  def set_range([rule | [next_rule | rest]]) do
+  # If the current rule is numeric and the next rule is numeric then
+  # the next rules value determines the upper bound of the validity
+  # of the current rule.
+  #
+  # ie.   "0": "one;"
+  #       "10": "ten;"
+  #
+  # Means that rule "0" is applied for values up to but not including "10"
+  defp set_range([rule | [next_rule | rest]]) do
     [%Rule{rule | :range => range_from_next_rule(rule.rule, next_rule.rule)}] ++ set_range([next_rule] ++ rest)
   end
-  def set_range([rule | []]) do
+  defp set_range([rule | []]) do
     [%Rule{rule | :range => :undefined}]
   end
   
-  def range_from_next_rule(rule, next_rule) do
+  defp range_from_next_rule(rule, next_rule) do
     with {_, ""} <- Integer.parse(rule),
          {_, ""} <- Integer.parse(next_rule) 
     do
       next_rule
     else
       :error -> :undefined
-      _ -> :error
     end
   end
 
--- lib/xml.ex
+++ lib/xml.ex
@@ -3,33 +3,42 @@ defmodule Xml do
   Record.defrecord :xmlAttribute, Record.extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
   Record.defrecord :xmlElement,   Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
   Record.defrecord :xmlText,      Record.extract(:xmlText,    from_lib: "xmerl/include/xmerl.hrl")
-  
-  def parse(file) do
-    {doc, _} = :xmerl_scan.file(String.to_char_list(file))
+  @type xml_node :: {:xmlElement, list}
+  @type xpath :: String.t
+    
+  @spec parse(String.t) :: xml_node | :error
+  def parse(filename) when is_binary(filename) do
+    {doc, _} = :xmerl_scan.file(String.to_charlist(filename))
     doc
   end
   
+  @spec all(xml_node, xpath) :: [xml_node]
   def all(node, path) do
     for child_element <- xpath(node, path) do
       child_element
     end
   end
 
+  @spec first(xml_node, xpath) :: xml_node | nil
   def first(node, path), do: node |> xpath(path) |> take_one
   defp take_one([head | _]), do: head
   defp take_one(_), do: nil
 
+  @spec node_name(xml_node) :: String.t | nil
   def node_name(nil), do: nil
   def node_name(node), do: elem(node, 1)
 
+  @spec attr(xml_node, String.t) :: String.t | nil
   def attr(node, name), do: node |> xpath("./@#{name}") |> extract_attr
   defp extract_attr([xmlAttribute(value: value)]), do: List.to_string(value)
   defp extract_attr(_), do: nil
 
+  @spec text(xml_node) :: String.t | nil
   def text(node), do: node |> xpath("./text()") |> extract_text
   defp extract_text([xmlText(value: value)]), do: List.to_string(value)
   defp extract_text(_x), do: nil
 
+  @spec xpath(xml_node, String.t) :: [xml_node]
   defp xpath(nil, _), do: []
   defp xpath(node, path) do
     :xmerl_xpath.string(to_char_list(path), node)
