a49dfb83222ef54e74d49181a374df1cfdaa6698
Kip Cole <kipcole9@gmail.com>
2016-08-24 08:01:51 +0800

    Refactor integer and decimal plural rule tests

--- test/decimal_plural_rule_test.exs
+++ test/decimal_plural_rule_test.exs
@@ -4,25 +4,26 @@ defmodule Decimal.PluralRule.Test do
   @sample_types [:decimal]
   @modules      [Cldr.Number.Cardinal, Cldr.Number.Ordinal]
 
-  Enum.each @modules, fn (module) ->
-    Enum.each module.configured_locales, fn (locale) ->
-      Enum.each module.plural_rules_for(locale), fn {category, rule} ->
-        Enum.each @sample_types, fn (sample_type) ->
-          Enum.each rule[sample_type] || [], fn
-          :ellipsis ->
-            true
-          {:.., _context, [from, to]} ->
-            test "#{inspect module}: Validate range #{inspect from}..#{inspect to} is in plural category #{inspect category} for locale #{inspect locale}" do
-              assert unquote(module).plural_rule(unquote(Macro.escape(from)), unquote(locale)) == unquote(category)
-              assert unquote(module).plural_rule(unquote(Macro.escape(to)), unquote(locale)) == unquote(category)
-            end
-          int ->
-            test "#{inspect module}: Validate number #{inspect int} is in plural category #{inspect category} for locale #{inspect locale}" do
-              assert unquote(module).plural_rule(unquote(Macro.escape(int)), unquote(locale)) == unquote(category)
-            end
-          end
+  for module           <- @modules,
+      locale           <- module.configured_locales,
+      {category, rule} <- module.plural_rules_for(locale),
+      sample_type      <- @sample_types,
+      one_rule         <- (rule[sample_type] || [])
+  do
+    case one_rule do
+      :ellipsis ->
+        true
+      {:.., _context, [from, to]} ->
+        test "#{inspect module}: Validate range #{inspect from}..#{inspect to} is in plural category #{inspect category} for locale #{inspect locale}" do
+          assert unquote(module).plural_rule(unquote(Macro.escape(from)), unquote(locale)) == unquote(category)
+          assert unquote(module).plural_rule(unquote(Macro.escape(to)), unquote(locale)) == unquote(category)
         end
-      end
+      int ->
+        test "#{inspect module}: Validate number #{inspect int} is in plural category #{inspect category} for locale #{inspect locale}" do
+          assert unquote(module).plural_rule(unquote(Macro.escape(int)), unquote(locale)) == unquote(category)
+        end
+      _ ->
+        nil
     end
-  end 
+  end
 end
--- test/integer_plural_rule_test.exs
+++ test/integer_plural_rule_test.exs
@@ -4,26 +4,25 @@ defmodule Integer.PluralRule.Test do
   @sample_types [:integer]
   @modules      [Cldr.Number.Cardinal, Cldr.Number.Ordinal]
 
-  Enum.each @modules, fn (module) ->
-    Enum.each module.configured_locales, fn (locale) ->
-      Enum.each module.plural_rules_for(locale), fn {category, rule} ->
-        Enum.each @sample_types, fn (sample_type) ->
-          Enum.each rule[sample_type] || [], fn
-          :ellipsis ->
-            true
-          {:.., _context, [from, to]} ->
-            Enum.each from..to, fn (int) ->
-              test "#{inspect module}: Validate number #{inspect int} in range #{inspect from}..#{inspect to} is in plural category #{inspect category} for locale #{inspect locale}" do
-                assert unquote(module).plural_rule(unquote(int), unquote(locale)) == unquote(category)
-              end
-            end
-          int ->
-            test "#{inspect module}: Validate number #{inspect int} is in plural category #{inspect category} for locale #{inspect locale}" do
-              assert unquote(module).plural_rule(unquote(int), unquote(locale)) == unquote(category)
-            end
+  for module           <- @modules,
+      locale           <- module.configured_locales,
+      {category, rule} <- module.plural_rules_for(locale),
+      sample_type      <- @sample_types,
+      one_rule         <- (rule[sample_type] || [])
+  do
+    case one_rule do
+      :ellipsis ->
+        true
+      {:.., _context, [from, to]} ->
+        Enum.each from..to, fn (int) ->
+          test "#{inspect module}: Validate number #{inspect int} in range #{inspect from}..#{inspect to} is in plural category #{inspect category} for locale #{inspect locale}" do
+            assert unquote(module).plural_rule(unquote(int), unquote(locale)) == unquote(category)
           end
         end
-      end
+      int ->
+        test "#{inspect module}: Validate number #{inspect int} is in plural category #{inspect category} for locale #{inspect locale}" do
+          assert unquote(module).plural_rule(unquote(int), unquote(locale)) == unquote(category)
+        end
     end
-  end 
+  end
 end
