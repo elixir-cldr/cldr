a7b391e96e88d5b05fdd4a6fbcdcb0c4ec503627
Kip Cole <kipcole9@gmail.com>
2016-08-28 21:41:58 +0800

    Add additional tests to improve coverage

---
+++ test/support_map_test.exs
@@ -0,0 +1,29 @@
+defmodule Support.Map.Test do
+  use ExUnit.Case
+
+  test "that map keys are underscored" do
+    assert Cldr.Map.underscore_keys(%{"thisKey" => "value"}) ==
+           %{"this_key" => "value"}
+  end
+
+  test "that map keys are atomised" do
+    assert Cldr.Map.atomize_keys(%{"thisKey" => "value"}) ==
+           %{thisKey: "value"}
+  end
+
+  test "that nested map keys are atomised" do
+    test_map = %{
+      "key" => %{
+        "nested" => "value"
+      }
+    }
+
+    test_result = %{
+      key: %{
+        nested: "value"
+      }
+    }
+
+    assert Cldr.Map.atomize_keys(test_map) == test_result
+  end
+end
