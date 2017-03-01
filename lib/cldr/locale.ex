d04a8a4b1cf4797710575ce0b45a067c284ed4e2
Kip Cole <kipcole9@gmail.com>
2017-03-01 22:22:45 +0800

    Add doc for Cldr.Locale.get_locale

--- lib/cldr/locale.ex
+++ lib/cldr/locale.ex
@@ -4,6 +4,14 @@ defmodule Cldr.Locale do
   """
   @type t :: binary
 
+  @doc """
+  Returns the map representation of a locale definition
+
+  A locale is defined by a map of definitions that are used to underpin the generation
+  of a set of functions that provide the public API.
+
+  * `locale` is any locale returned by `Cldr.known_locales/0`
+  """
   @spec get_locale(Locale.t) :: Map.t
   Enum.each Cldr.known_locales(), fn locale_name ->
     locale = Cldr.Config.get_locale(locale_name)
