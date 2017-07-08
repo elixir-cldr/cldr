848e549104eb1ad233c0e991a4b2505252d903e7
Kip Cole <kipcole9@gmail.com>
2017-07-08 10:02:12 +0900

    Make installation of a default locale other than 'en' work again.  Closes #11

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :ex_cldr,
-  default_locale: "en",
+  default_locale: "en-SG",
   locales: ["root", "fr", "en", "bs", "pl", "ru", "th", "he", "af", "af-NA"],
   gettext: Cldr.Gettext,
   precompile_number_formats: ["¤¤#,##0.##"],
