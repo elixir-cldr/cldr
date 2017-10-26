e7aafe38f4c96a200722800a7d6e5a74367e396e
Kip Cole <kipcole9@gmail.com>
2017-10-26 10:06:39 +0800

    Progress on the new language tag parser generator

--- mix.exs
+++ mix.exs
@@ -41,23 +41,11 @@ defmodule Cldr.Mixfile do
     [
       {:poison, "~> 2.1 or ~> 3.0"},
       {:decimal, "~> 1.4.1"},
-<<<<<<< 4868b5ef2db429e40ae50f6b95c52340193b5562
-<<<<<<< 5205470c6475eaa46495e2a06824501d647b4886
       {:gen_stage, "~> 0.12.2"},
       {:flow, "~> 0.11"},
-      {:ex_abnf, "~> 0.3.0"},
       {:ex_doc, "~> 0.18.1", only: [:dev, :docs]},
       {:excoveralls, "~> 0.7.4", only: :test, runtime: false},
-=======
-      {:ex_doc, "~> 0.18.1", only: [:dev, :docs]},
-      {:excoveralls, "~> 0.7.4", only: :test},
       {:abnf, path: "../abnf"},
->>>>>>> Progress on the new language tag parser generator
-=======
-      {:ex_doc, "~> 0.18.1", only: [:dev, :docs]},
-      {:excoveralls, "~> 0.7.4", only: :test},
-      {:abnf, path: "../abnf"},
->>>>>>> Rebase on master
       {:gettext, "~> 0.13.0", optional: true},
       {:stream_data, "~> 0.3.0", only: :test},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false}
