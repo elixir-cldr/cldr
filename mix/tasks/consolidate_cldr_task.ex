1cdfb48e21cc0813d586e57f00a9c45faf4949ba
Kip Cole <kipcole9@gmail.com>
2017-11-17 19:06:02 +1100

    Don't add mix tasks if the raw CLDR data isn't present (which is true for the package on hexx)

--- mix/tasks/consolidate_cldr_task.ex
+++ mix/tasks/consolidate_cldr_task.ex
@@ -1,17 +1,19 @@
-defmodule Mix.Tasks.Cldr.Consolidate do
-  @moduledoc """
-  Mix task to consolidate the cldr data into a set of files, one file per
-  CLDR locale.
-  """
+if File.exists?(Cldr.Config.download_data_dir) do
+  defmodule Mix.Tasks.Cldr.Consolidate do
+    @moduledoc """
+    Mix task to consolidate the cldr data into a set of files, one file per
+    CLDR locale.
+    """
 
-  use Mix.Task
+    use Mix.Task
 
-  @shortdoc "Consolidate cldr json data into a single per-locale set of files"
+    @shortdoc "Consolidate cldr json data into a single per-locale set of files"
 
-  def run(_) do
-    System.put_env("DEV", "true")
-    Cldr.Consolidate.consolidate_locales()
+    @doc false
+    def run(_) do
+      System.put_env("DEV", "true")
+      Cldr.Consolidate.consolidate_locales()
+    end
   end
 end
-
 
--- mix/tasks/install_cldr_data.ex
+++ mix/tasks/install_cldr_data.ex
@@ -1,16 +1,18 @@
-defmodule Mix.Tasks.Cldr.Install.Locales do
-  @moduledoc """
-  Installs the cldr core data and configured locales into the application.  By
-  default it installs into the ./priv/cldr directory.
-  """
+if File.exists?(Cldr.Config.download_data_dir) do
+  defmodule Mix.Tasks.Cldr.Install.Locales do
+    @moduledoc """
+    Installs the cldr core data and configured locales into the application.  By
+    default it installs into the ./priv/cldr directory.
+    """
 
-  use Mix.Task
+    use Mix.Task
 
-  @shortdoc "Install all configured `Cldr` locales."
+    @shortdoc "Install all configured `Cldr` locales."
 
-  def run(_) do
-    Cldr.Install.install_known_locale_names
+    @doc false
+    def run(_) do
+      Cldr.Install.install_known_locale_names
+    end
+
   end
-
 end
-
