e4b14c2d9bf4126549d332646bd8cf713e9d49ed
Kip Cole <kipcole9@gmail.com>
2018-02-25 21:11:38 +0800

    Ensure outfilename is language_tags.ebin

--- mix/tasks/generate_language_tags.ex
+++ mix/tasks/generate_language_tags.ex
@@ -15,7 +15,7 @@ defmodule Mix.Tasks.Cldr.GenerateLanguageTags do
     end
 
     # We set the gettext locale name to nil because we can't tell in advance
-    # waht the gettext locale name will be (if any)
+    # what the gettext locale name will be (if any)
     language_tags =
       for locale_name <- Cldr.all_locale_names() do
         language_tag = Map.put(Cldr.Locale.new!(locale_name), :gettext_locale_name, nil)
@@ -23,7 +23,7 @@ defmodule Mix.Tasks.Cldr.GenerateLanguageTags do
       end
       |> Enum.into(%{})
 
-    output_path = Path.join(Cldr.Config.source_data_dir(), "language_tags")
+    output_path = Path.join(Cldr.Config.source_data_dir(), "language_tags.ebin")
     File.write!(output_path, :erlang.term_to_binary(language_tags))
   end
 end
