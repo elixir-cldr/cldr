99a080efec0dba8990b81d1d095e10f3e25b9212
Dan Schultzer <dan@dreamconception.com>
2018-05-16 12:12:42 -0700

    Refactor tests

--- test/locale_parser_test.exs
+++ test/locale_parser_test.exs
@@ -4,22 +4,6 @@ defmodule CldrLocaleParserTest do
 
   doctest Cldr.LanguageTag.Parser
 
-  @invalid_locale_name "zz"
-  @invalid_territory_code :AA
-  @invalid_currency_code :AAA
-
-  test "That invalid locale name is invalid" do
-    assert @invalid_locale_name not in Cldr.known_locale_names()
-  end
-
-  test "That invalid territory code is invalid" do
-    assert @invalid_territory_code not in Cldr.known_territories()
-  end
-
-  test "That invalid currency code is invalid" do
-    assert @invalid_currency_code not in Cldr.known_currencies()
-  end
-
   @language_codes [
     # "und-Cyrl-t-und-latn-m0-ungegn-2007",
     "en-US-x-twain",
@@ -37,15 +21,54 @@ defmodule CldrLocaleParserTest do
     "und-u-cu-usd",
     "en_US_u_tz_uslax_va_posix",
     "th_TH_u_ca_gregory_nu_thai",
-    "zh_Hant_TW_u_co_big5han",
-    "#{@invalid_locale_name}-US", # Invalid language
-    "en-#{@invalid_territory_code}", # Invalid country
-    "en-US-u-cu-#{@invalid_currency_code}" # Invalid currency
+    "zh_Hant_TW_u_co_big5han"
   ]
 
   for code <- @language_codes do
     test "That #{inspect(code)} parses without error" do
       assert {:ok, _} = Parser.parse(unquote(code))
     end
+  end
+
+  test "That invalid locale string returns error" do
+    assert {:error, _error} = Parser.parse("-invalid")
+  end
+
+  test "That invalid language code is handled" do
+    assert {:ok, language_tag} = Parser.parse("aaaaa")
+    assert is_nil(language_tag.cldr_locale_name)
+    assert language_tag.language == "aaaaa"
+  end
+
+  test "That nonexistent language code is handled" do
+    assert {:ok, language_tag} = Parser.parse("zz")
+    assert is_nil(language_tag.cldr_locale_name)
+    assert language_tag.language == "zz"
+  end
+
+  test "That invalid territory code is handled" do
+    assert {:ok, language_tag} = Parser.parse("en-AAAA")
+    assert language_tag.language == "en"
+    assert is_nil(language_tag.territory)
+  end
+
+  test "That nonexistent territory code is handled" do
+    assert {:ok, language_tag} = Parser.parse("en-AA")
+    assert language_tag.language == "en"
+    assert language_tag.territory == "AA"
+  end
+
+  test "That invalid currency code is handled" do
+    assert {:ok, language_tag} = Parser.parse("en-US-u-cu-AAAAAA")
+    assert language_tag.language == "en"
+    assert language_tag.territory == "US"
+    assert is_nil(language_tag.locale.currency)
+  end
+
+  test "That nonexistent currency code is handled" do
+    assert {:ok, language_tag} = Parser.parse("en-US-u-cu-AAA")
+    assert language_tag.language == "en"
+    assert language_tag.territory == "US"
+    assert is_nil(language_tag.locale.currency)
   end
 end
