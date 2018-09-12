833f27ef346b601b6272be78ce7669128706998c
Kip Cole <kipcole9@gmail.com>
2018-09-12 10:20:49 +0900

    Update language tag benchee script

--- bench/langauge_tag_parsing.ex
+++ bench/langauge_tag_parsing.ex
@@ -1,16 +1,24 @@
 defmodule Cldr.Bench.LanguageTag do
-  @path "bench/language_tag.benchee"
+  @path "bench/results/language_tag"
+
   def run do
-    version = Application.spec(:ex_cldr) |> Keyword.get(:vsn)
+    version =
+      :ex_cldr
+      |> Application.spec
+      |> Keyword.get(:vsn)
 
     Benchee.run(%{
       "parse language tag in CLDR #{version}" => fn ->
         Cldr.LanguageTag.parse("en-Latn-US-u-ca-gregory-cu-usd")
        end,
-    }, [save: [path: @path, tag: version]])
+    }, save: [path: path(version), tag: version])
   end
 
   def compare do
-    Benchee.run(%{}, load: @path)
+    Benchee.run(%{}, load: "#{@path}*")
+  end
+
+  defp path(version) do
+    "#{@path}_#{version}.benchee"
   end
 end
