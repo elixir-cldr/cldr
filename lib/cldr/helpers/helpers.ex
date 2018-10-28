09ea0c9db8ef76a2ac819f016289fe10aab78fbf
Kip Cole <kipcole9@gmail.com>
2018-10-29 01:21:23 +0800

    Extract utils into their own package cldr_utils

--- lib/cldr/helpers/helpers.ex
+++
@@ -1,6 +0,0 @@
-defmodule Cldr.Helpers do
-  def empty?([]), do: true
-  def empty?(%{} = map) when map == %{}, do: true
-  def empty?(nil), do: true
-  def empty?(_), do: false
-end
--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule Cldr.Mixfile do
       {:phoenix, "~> 1.3", optional: true},
       {:plug, "~> 1.4", optional: true},
       {:sweet_xml, "~> 0.6", optional: true},
-      {:benchee, "~> 0.13", only: :dev, runtime: false}
+      {:benchee, "~> 0.13", only: :dev, runtime: false},
+      {:cldr_utils, "~> 2.0"}
     ]
   end
 
