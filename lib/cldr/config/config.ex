f891a0ea4c29ffafbd0a8279b5862f3c9a8adbcd
Kip Cole <kipcole9@gmail.com>
2018-11-23 01:52:19 +0800

    Fix decimal_format_list

--- lib/cldr/config/config.ex
+++ lib/cldr/config/config.ex
@@ -1487,6 +1487,7 @@ defmodule Cldr.Config do
     |> Map.values()
     |> Enum.map(&Map.delete(&1, :currency_spacing))
     |> Enum.map(&Map.delete(&1, :currency_long))
+    |> Enum.map(&Map.delete(&1, :other))
     |> Enum.map(&Map.values/1)
     |> List.flatten()
     |> Enum.reject(&is_integer/1)
