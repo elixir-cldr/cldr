6eede9c7b9d6d2998f23c8db30ec1cf09b1fbc9f
Kip Cole <kipcole9@gmail.com>
2021-02-28 16:24:25 +0800

    Remove spurious IO.inspect/1 call

--- lib/cldr/config/config.ex
+++ lib/cldr/config/config.ex
@@ -1765,7 +1765,6 @@ defmodule Cldr.Config do
       |> Path.join(@grammatical_features_file)
       |> File.read!()
       |> json_library().decode!
-      |> IO.inspect
 
     data
     |> Enum.map(fn {k, v} ->
