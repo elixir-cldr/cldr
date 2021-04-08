bbffbdc252356ff2704fad8b972d16cd5d69bc74
Kip Cole <kipcole9@gmail.com>
2021-04-08 21:16:17 +1000

    Add --force-locale-download option to mix task cldr.install.locales

--- mix/tasks/install_cldr_data.ex
+++ mix/tasks/install_cldr_data.ex
@@ -1,34 +1,55 @@
-if File.exists?(Cldr.Config.download_data_dir()) do
-  defmodule Mix.Tasks.Cldr.Install.Locales do
-    @moduledoc """
-    Installs the cldr core data and configured locales into the application.  By
-    default it installs into the ./priv/cldr directory.
+defmodule Mix.Tasks.Cldr.Install.Locales do
+  @moduledoc """
+  Installs the cldr core data and configured locales into the application.  By
+  default it installs into the ./priv/cldr directory.
 
-    ## Arguments
+  ## Arguments
 
-    * `backend` is the name of any backend module for which
-      the locales will be installed. This is a required parameter
-      and there is no default.
+  * `backend` is the name of any backend module for which
+    the locales will be installed. This is a required parameter
+    and there is no default.
 
-    ## Example
+  ## Options
 
-        % mix cldr.install.locales MyApp.Cldr
+  * `--force-locale-download` will force locales
+    to be downloaded even if they are already
+    available.
 
-    """
+  ## Examples
 
-    use Mix.Task
+      % mix cldr.install.locales MyApp.Cldr
+      % mix cldr.install.locales --force-locale-download MyApp.Cldr
 
-    @shortdoc "Install all configured `Cldr` locales."
+  """
 
-    @doc false
-    def run([]) do
-      raise ArgumentError, "A Cldr backend module must be provided"
-    end
+  use Mix.Task
 
-    def run([backend]) do
-      module = Module.concat([backend])
-      config = module.__cldr__(:config)
-      Cldr.Install.install_known_locale_names(config)
+  @shortdoc "Install all configured `Cldr` locales for a given backend."
+
+  @doc false
+  def run([]) do
+    raise ArgumentError, "A Cldr backend module name must be provided"
+  end
+
+  @options [strict: [force_locale_download: :boolean]]
+
+  def run(args) do
+    case OptionParser.parse!(args, @options) do
+      {_options, [] = backend} ->
+        run(backend)
+
+      {options, backend} ->
+        module = Module.concat(backend)
+        config = module.__cldr__(:config)
+
+        config =
+          if Keyword.get(options, :force_locale_download) do
+            Map.put(config, :force_locale_download, true)
+          else
+            config
+          end
+
+        Cldr.Install.install_known_locale_names(config)
     end
   end
 end
