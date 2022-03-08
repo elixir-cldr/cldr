4663076b5b348fbd522a99322f565ebc6520d674
Kip Cole <kipcole9@gmail.com>
2022-03-08 18:56:45 +0800

    Remove one more spurious check for downloaded data

--- mix/tasks/consolidate_cldr_task.ex
+++ mix/tasks/consolidate_cldr_task.ex
@@ -1,4 +1,4 @@
-if File.exists?(Cldr.Config.download_data_dir()) && Cldr.Code.ensure_compiled?(Cldr.Consolidate) do
+if Cldr.Config.production_data_location && Cldr.Code.ensure_compiled?(Cldr.Consolidate) do
   defmodule Mix.Tasks.Cldr.Consolidate do
     @moduledoc """
     Mix task to consolidate the cldr data into a set of files, one file per
