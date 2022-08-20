4c0b25395ac86cce21041f6ada981fbe95802926
Kip Cole <kipcole9@gmail.com>
2022-08-20 13:43:17 +1000

    delegate to Cldr.Http.get for locale installation

--- lib/cldr/install.ex
+++ lib/cldr/install.ex
@@ -91,128 +91,22 @@ defmodule Cldr.Install do
     output_file_name = locale_output_file_name(locale_name, config)
     url = String.to_charlist("#{base_url()}#{locale_filename(locale_name)}")
 
-    case :httpc.request(:get, {url, headers()}, https_opts(), []) do
-      {:ok, {{_version, 200, 'OK'}, _headers, body}} ->
+    case Cldr.Http.get(url) do
+      {:ok, body} ->
         output_file_name
         |> File.write!(body)
 
         Logger.bare_log(:info, "Downloaded locale #{inspect(locale_name)}")
         {:ok, output_file_name}
 
-      {_, {{_version, code, message}, _headers, _body}} ->
-        Logger.bare_log(
-          :error,
-          "Failed to download locale #{inspect(locale_name)} from #{url}. " <>
-            "HTTP Error: (#{code}) #{inspect(message)}"
-        )
-
-        {:error, code}
-
-      {:error, {:failed_connect, [{_, {host, _port}}, {_, _, sys_message}]}} ->
-        Logger.bare_log(
-          :error,
-          "Failed to connect to #{inspect(host)} to download " <>
-            "locale #{inspect(locale_name)}. Reason: #{inspect(sys_message)}"
-        )
-
-        {:error, sys_message}
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
   defp locale_output_file_name(locale_name, config) do
     [client_locales_dir(config), "/", locale_filename(locale_name)]
     |> :erlang.iolist_to_binary()
-  end
-
-  defp headers do
-    # [{'Connection', 'close'}]
-    []
-  end
-
-  @certificate_locations [
-                           # Configured cacertfile
-                           Application.compile_env(:ex_cldr, :cacertfile),
-
-                           # Populated if hex package CAStore is configured
-                           if(Code.ensure_loaded?(CAStore), do: CAStore.file_path()),
-
-                           # Populated if hex package certfi is configured
-                           if(Code.ensure_loaded?(:certifi),
-                             do: :certifi.cacertfile() |> List.to_string()
-                           ),
-
-                           # Debian/Ubuntu/Gentoo etc.
-                           "/etc/ssl/certs/ca-certificates.crt",
-
-                           # Fedora/RHEL 6
-                           "/etc/pki/tls/certs/ca-bundle.crt",
-
-                           # OpenSUSE
-                           "/etc/ssl/ca-bundle.pem",
-
-                           # OpenELEC
-                           "/etc/pki/tls/cacert.pem",
-
-                           # CentOS/RHEL 7
-                           "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem",
-
-                           # Open SSL on MacOS
-                           "/usr/local/etc/openssl/cert.pem",
-
-                           # MacOS & Alpine Linux
-                           "/etc/ssl/cert.pem"
-                         ]
-                         |> Enum.reject(&is_nil/1)
-
-  def certificate_store do
-    @certificate_locations
-    |> Enum.find(&File.exists?/1)
-    |> raise_if_no_cacertfile
-    |> :erlang.binary_to_list()
-  end
-
-  defp raise_if_no_cacertfile(nil) do
-    raise RuntimeError, """
-    No certificate trust store was found.
-    Tried looking for: #{inspect(@certificate_locations)}
-
-    A certificate trust store is required in
-    order to download locales for your configuration.
-
-    Since ex_cldr could not detect a system
-    installed certificate trust store one of the
-    following actions may be taken:
-
-    1. Install the hex package `castore`. It will
-       be automatically detected after recompilation.
-
-    2. Install the hex package `certifi`. It will
-       be automatically detected after recomilation.
-
-    3. Specify the location of a certificate trust store
-       by configuring it in `config.exs`:
-
-       config :ex_cldr,
-         cacertfile: "/path/to/cacertfile",
-         ...
-
-    """
-  end
-
-  defp raise_if_no_cacertfile(file) do
-    file
-  end
-
-  defp https_opts do
-    [
-      ssl: [
-        verify: :verify_peer,
-        cacertfile: certificate_store(),
-        customize_hostname_check: [
-          match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-        ]
-      ]
-    ]
   end
 
   # Builds the base url to retrieve a locale file from github.
