f7405418de1f74e395961a3670a2b98bbe0dc88c
Kip Cole <kipcole9@gmail.com>
2023-02-25 04:39:42 +0800

    Refactor Gettext tests

--- test/gettext_test.exs
+++ test/gettext_test.exs
@@ -14,4 +14,23 @@ defmodule Cldr.Gettext.Test do
     assert T.lngettext("it", "default", nil, "One new email", "%{count} new emails", 2, %{}) ==
              {:ok, "2 nuove email"}
   end
+
+  test "An exception is raised when a Gettext backend module encounters an unknown locale" do
+    assert_raise Gettext.Plural.UnknownLocaleError, ~r/gsw/, fn ->
+      defmodule TestGettext.GettextUnknown do
+        @moduledoc """
+        Implements a Gettext-compatible module that does not have the base
+        language locale configured for "gsw" and will therefore raise.
+
+        See https://hexdocs.pm/gettext/Gettext.Plural.html#module-language-and-territory
+        for an explanation of which the exception is raised (its because we have configured
+        "gsw_CH" but not "gsw" and Gettext forwards plurals to the base language).
+
+        """
+        use Gettext,
+          otp_app: Cldr.Config.app_name(),
+          priv: "priv/gettext_unknown"
+      end
+    end
+  end
 end
