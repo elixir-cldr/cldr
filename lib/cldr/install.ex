e768dbe8dc45e0e194dab4c96613c1fa5ce2e2e3
Kip Cole <kipcole9@gmail.com>
2023-09-15 11:57:39 +0800

    CLDR 44 data after freeze

--- lib/cldr/install.ex
+++ lib/cldr/install.ex
@@ -189,13 +189,14 @@ defmodule Cldr.Install do
       |> Cldr.Locale.Loader.read_locale_file!()
       |> Config.json_library().decode!
       |> Map.get("version")
+      |> Version.parse!
 
-    stale? = is_nil(version) || Version.compare(Cldr.version(), Version.parse!(version)) != :eq
+    stale? = is_nil(version) || Version.compare(Cldr.version(), version) != :eq
 
     if stale? do
       Logger.bare_log(
         :info,
-        "Locale data for #{inspect(locale_name)} is stale. " <>
+        "Locale data for #{inspect(locale_name)} is stale. Cldr version is #{Cldr.version}, locale version is #{version}" <>
           "Updated locale data will be downloaded."
       )
     end
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Cldr.Mixfile do
   use Mix.Project
 
-  @version "2.37.4"
+  @version "2.38.0-dev"
 
   def project do
     [
