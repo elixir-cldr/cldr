f2bcabf3a4ffb6456247d47513896236d63adc88
Kip Cole <kipcole9@gmail.com>
2025-06-29 22:15:59 +1000

    Add additional substitution pattern

--- lib/cldr/substitution.ex
+++ lib/cldr/substitution.ex
@@ -125,6 +125,10 @@ defmodule Cldr.Substitution do
     [item_0, string1, item_1, string2]
   end
 
+  def substitute([item_0, item_1], [1, string1, 0, string2]) do
+    [item_0, string1, item_1, string2]
+  end
+
   # Takes care of the common case where there are three parameters separated
   # by strings.
   def substitute([item_0, item_1, item_2], [0, string_1, 1, string_2, 2])
