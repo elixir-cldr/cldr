4fa272b5b559106caba302c4962e8619208ba680
Kip Cole <kipcole9@gmail.com>
2025-10-05 10:01:44 +1100

    CLDR48 beta 2

--- lib/cldr/config/config.ex
+++ lib/cldr/config/config.ex
@@ -2470,6 +2470,7 @@ defmodule Cldr.Config do
     |> Enum.map(&Map.delete(&1, :currency_spacing))
     |> Enum.map(&Map.delete(&1, :currency_long))
     |> Enum.map(&Map.delete(&1, :currency_with_iso))
+    |> Enum.map(&Map.delete(&1, :rational))
     |> Enum.map(&Map.delete(&1, :other))
     |> Enum.map(&Map.values/1)
     |> List.flatten()
