6d4e65c307f07f6f1452bc301ecfd749e428e174
Kip Cole <kipcole9@gmail.com>
2025-10-08 09:46:04 +1100

    quote preference can be a string?

--- lib/cldr/backend/cldr_backend.ex
+++ lib/cldr/backend/cldr_backend.ex
@@ -503,7 +503,8 @@ defmodule Cldr.Backend do
       defp quote_preference(marks, preference) when is_map_key(marks, preference),
         do: Map.fetch!(marks, preference)
 
-      defp quote_preference(marks, _preference), do: marks.default
+      defp quote_preference(%{default: default}, _preference), do: default
+      defp quote_preference(default, _preference) when is_binary(default), do: default
 
       @doc """
       Add locale-specific ellipsis to a string.
